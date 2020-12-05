045891bf4e4c6962b584c8d0685187b6a345c0a6
shuyanwang <shuyan.wang@west.cmu.edu>
2020-12-05 15:21:04 -0500

    protect support request resource and reset resource

--- resources/ResetResource.py
+++ resources/ResetResource.py
@@ -8,16 +8,25 @@ from services.PaymentMethodService import *
 from services.RequestService import *
 from services.RatingService import *
 from services.SupportRequestService import *
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_identity
+)
 
 
 class ResetResource(Resource):
+    @jwt_required
     def get(self):
-        reset_helpers()
-        reset_needers()
-        reset_cars()
-        reset_bank_accounts()
-        reset_payment_methods()
-        reset_requests()
-        reset_ratings()
-        reset_support_requests()
-        return jsonify({"message": "reset succeeded"})
+        jwt_identity = get_jwt_identity()
+        if jwt_identity['role'] == 'admin':
+            reset_helpers()
+            reset_needers()
+            reset_cars()
+            reset_bank_accounts()
+            reset_payment_methods()
+            reset_requests()
+            reset_ratings()
+            reset_support_requests()
+            return jsonify({"message": "reset succeeded"})
+        else:
+            return 'only admin can reset', 403
--- resources/SupportRequestResource.py
+++ resources/SupportRequestResource.py
@@ -1,6 +1,10 @@
 from flask_restful import Resource
 from flask import jsonify, request
 from services.SupportRequestService import *
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_identity
+)
 
 
 class SupportRequestResource(Resource):
@@ -10,30 +14,54 @@ class SupportRequestResource(Resource):
     query param: filterBy, filterValue (e.g. filterBy=cities, filterValue=San Francisco)
     query param: pageSize, page (pageSize: num helpers in the same page, page, which page, starting from 1)
     """
+    @jwt_required
     def get(self, support_request_id=None):
-        if support_request_id:
-            return jsonify((get_support_request_by_id(support_request_id)))
+        jwt_identity = get_jwt_identity()
+        if jwt_identity['role'] == 'admin':
+            if support_request_id:
+                return jsonify((get_support_request_by_id(support_request_id)))
+            else:
+                all_requests = get_all_support_requests(
+                    request.args.get('filterBy'),
+                    request.args.get('filterValue'),
+                    request.args.get('sortBy'),
+                    request.args.get('sortOrder'),
+                    request.args.get('pageSize'),
+                    request.args.get('page')
+                )
+                return jsonify(all_requests)
         else:
-            all_requests = get_all_support_requests(
-                request.args.get('filterBy'),
-                request.args.get('filterValue'),
-                request.args.get('sortBy'),
-                request.args.get('sortOrder'),
-                request.args.get('pageSize'),
-                request.args.get('page')
-            )
-            return jsonify(all_requests)
+            return 'only admin can get support requests', 403
 
+    @jwt_required
     def patch(self, support_request_id=None):
         if support_request_id:
-            return jsonify(update_support_request(support_request_id, request.json))
+            jwt_identity = get_jwt_identity()
+            if jwt_identity['role'] == 'admin':
+                the_support_request = get_support_request_by_id(support_request_id)
+                if the_support_request:
+                    return jsonify(update_support_request(support_request_id, request.json))
+                else:
+                    return 'support request not found', 404
+            else:
+                return 'only admin can update support requests', 403
         else:
             return 'support_request_id is needed for updating a request'
 
+    @jwt_required
     def post(self):
         the_created_support_request = create_support_request_in_db(request.json)
         return jsonify(the_created_support_request)
 
+    @jwt_required
     def delete(self, support_request_id=None):
-        delete_support_request(support_request_id)
-        return jsonify({"message": "delete success"})
+        jwt_identity = get_jwt_identity()
+        if jwt_identity['role'] == 'admin':
+            the_support_request = get_support_request_by_id(support_request_id)
+            if the_support_request:
+                delete_support_request(support_request_id)
+                return jsonify({"message": "delete success"})
+            else:
+                return 'support request not found', 404
+        else:
+            return 'only admin can delete support requests', 403
